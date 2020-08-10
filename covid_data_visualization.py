c4a3b7185db1cd220b96701935bd5a5a3c6cabee
aortiz24 <aortiz1865@gmail.com>
2020-08-09 19:41:42 -0500

    removed data visualization script

--- covid_data_visualization.py
+++
@@ -1,9 +0,0 @@
-#Data visualization
-import matplotlib.pyplot as plt
-
-#plot known data
-day_new=np.arange(1,10)
-plt.plot(day_new,timeseries_data)
-#plot predicted data
-day_pred=np.arange(10,20)
-plt.plot(day_pred,lst_output)
