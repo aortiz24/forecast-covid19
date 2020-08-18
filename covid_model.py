0ccd49f7b9a0fd2f8766aee01ad558b9311fb68a
aortiz24 <aortiz1865@gmail.com>
2020-08-18 13:08:14 -0500

    updated code in python script with normalization, Dropout function to combat overfitting, & a data visualization element

--- covid_model.py
+++ covid_model.py
@@ -1,10 +1,12 @@
 #import libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
 from sklearn.model_selection import TimeSeriesSplit
 
 ##Data Preparation
@@ -25,15 +27,18 @@ def prepare_data(time_series_data, n_features):
 
 #define input sequence
 tx_cases = pd.read_csv("Texas_covid_data.csv", usecols=["cases"])
-print(tx_cases)
+#perform data normalization 
+#daily number of Texas covid cases converted to fit between 0 and 1
+scaler = MinMaxScaler(feature_range = (0, 1))
+tx_cases_scaled = scaler.fit_transform(tx_cases)
 #indicate number of features
 n_steps = 3
 # split into samples
-X, y = prepare_data(tx_cases, n_steps)
+X, y = prepare_data(tx_cases_scaled, n_steps)
 
 #inspect the shape of X
 #(number of rows, number of columns)
-X.shape
+print(X.shape)
 
 # X is in 2 dimensions. For a LSTM model X needs to be in 3 dimensions
 #reshape X by adding the dimension 'n_element'
@@ -44,9 +49,15 @@ X = X.reshape((X.shape[0], X.shape[1], n_element))
 # define model
 model = Sequential()
 model.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(n_steps, n_element)))
-model.add(LSTM(50, activation='relu'))
+model.add(Dropout(0.2)) #prevents overfitting
+model.add(LSTM(50, activation='relu', return_sequences=True))
+model.add(Dropout(0.2)) #prevents overfitting
+model.add(LSTM(50, activation='relu', return_sequences=True))
+model.add(Dropout(0.2)) #prevents overfitting
+model.add(LSTM(50))
+model.add(Dropout(0.2)) #prevents overfitting
 model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
+model.compile(optimizer='adam', loss='mean_squared_error')
 
 #split X, y variables into training & test data sets for each variable
 tscv = TimeSeriesSplit()
@@ -67,4 +78,16 @@ model.evaluate(x=X_test, verbose=1)
 
 ##Model Prediction
 #predict using numpy array of daily COVID-19 cases in Texas
-model.predict(x=X)
+prediction = model.predict(x=X)
+#reverse normalization of prediction data
+predictions = scaler.inverse_transform(prediction)
+
+#plot actual and predicted data
+plt.figure(figsize=(10,6))
+plt.plot(tx_cases, color='blue', label='Actual Daily COVID-19 Cases')
+plt.plot(predictions, color='red', label='Predicted Daily COVID-19 Cases')
+plt.title('Daily COVID-19 Cases Prediction')
+plt.xlabel('Date')
+plt.ylabel('Daily COVID-19 Cases')
+plt.legend()
+plt.show()
